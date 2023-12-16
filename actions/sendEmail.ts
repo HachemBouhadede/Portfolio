987c516622d79195906f4abe72dbcc037c22fef9
HachemBouhadede <hachembouhadede@gmail.com>
2023-12-16 16:31:13 +0100

    few changes

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -26,7 +26,7 @@ export const sendEmail = async (formData: FormData) => {
   let data;
   try {
     data = await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "Portfolio Contact Form <onboarding@resend.dev>",
       to: "hachembouhadede@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
